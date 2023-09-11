b2d00ffb14ff9873d42ccaa1b786f89f26359c98
‪Ori David Haknin <133655211+Haknin@users.noreply.github.com>
2023-09-11 21:06:26 +0300

    Update helmstatus.sh

--- helm/helmstatus.sh
+++ helm/helmstatus.sh
@@ -5,7 +5,7 @@ command_output=$(helm status my-gcs-repo 2>&1)
 expected_error="Error: release: not found"
 
 if [ "$command_output" = "$expected_error" ]; then
-    helm install my-gcs-repo crypto-site/helm/crypto_flask_haknin/
+    helm install my-gcs-repo project-crypto/helm/crypto_flask_haknin/
 else
-    helm upgrade --recreate-pods my-gcs-repo crypto-site/helm/crypto_flask_haknin/
+    helm upgrade --recreate-pods my-gcs-repo project-crypto/helm/crypto_flask_haknin/
 fi
