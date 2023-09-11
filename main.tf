c42bbdb8961938ca3ad5a86e1db8076f88a0c29a
haknin <hakninori@gmail.com>
2023-09-11 20:13:07 +0300

    main tf change

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
 provider "google" {
   project = "encoded-adviser-393206"
-  region = "europe-central2"
+  region = "us-central1-c"
   credentials = "encoded-adviser-393206-32637e9b46fc.json"
 }
 resource "google_container_cluster" "test" {
   name = "cluster-test"
-  location = "europe-central2"
+  location = "us-central1-c"
   enable_autopilot = true
 }
 resource "google_container_cluster" "flask" {
